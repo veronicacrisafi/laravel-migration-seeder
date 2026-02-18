36e2d49944ba7d7e9fb3b196d87c39a31988b014
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-18 17:28:55 +0100

    Aggiunta la tabella per visualizzare i dati dei treni nella vista index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,32 @@
 @section('content')
     <table>
         <thead>
-            <h1>treni</h1>
+            <tr>
+                <th>Azienda</th>
+                <th>Stazione di partenza</th>
+                <th>Stazione di arrivo</th>
+                <th>Orario di partenza</th>
+                <th>Orario di arrivo</th>
+                <th>Codice treno</th>
+                <th>Totale carrozze</th>
+                <th>In orario</th>
+                <th>Cancellato</th>
+            </tr>
         </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>{{ $train->azienda }}</td>
+                    <td>{{ $train->stazione_di_partenza }}</td>
+                    <td>{{ $train->stazione_di_arrivo }}</td>
+                    <td>{{ $train->orario_di_partenza }}</td>
+                    <td>{{ $train->orario_di_arrivo }}</td>
+                    <td>{{ $train->codice_treno }}</td>
+                    <td>{{ $train->totale_carrozze }}</td>
+                    <td>{{ $train->in_orario ? 'Sì' : 'No' }}</td>
+                    <td>{{ $train->cancellato ? 'Sì' : 'No' }}</td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
 @endsection
