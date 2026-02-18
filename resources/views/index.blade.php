5ddd641f9ff4ad1f2a2be4b5b5f3a11e42110a71
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-18 18:17:30 +0100

    Aggiunti stili e font personalizzati per la visualizzazione dei dati dei treni nella vista index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,31 +5,31 @@
         <table class="table table-info table-hover text-center">
             <thead>
                 <tr>
-                    <th scope="col">Azienda</th>
-                    <th scope="col">Stazione di partenza</th>
-                    <th scope="col">Stazione di arrivo</th>
-                    <th scope="col">Orario di partenza</th>
-                    <th scope="col">Orario di arrivo</th>
-                    <th scope="col">Codice treno</th>
-                    <th scope="col">Totale carrozze</th>
-                    <th scope="col">In orario</th>
-                    <th scope="col">Cancellato</th>
+                    <th scope="col" class="fs-4 py-5">Azienda</th>
+                    <th scope="col" class="fs-4 py-5">Stazione di partenza</th>
+                    <th scope="col" class="fs-4 py-5">Stazione di arrivo</th>
+                    <th scope="col" class="fs-4 py-5">Orario di partenza</th>
+                    <th scope="col" class="fs-4 py-5">Orario di arrivo</th>
+                    <th scope="col" class="fs-4 py-5">Codice treno</th>
+                    <th scope="col" class="fs-4 py-5">Totale carrozze</th>
+                    <th scope="col" class="fs-4 py-5">In orario</th>
+                    <th scope="col" class="fs-4 py-5">Cancellato</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($trains as $train)
                     <tr>
-                        <td>{{ $train->azienda }}</td>
-                        <td>{{ $train->stazione_di_partenza }}</td>
-                        <td>{{ $train->stazione_di_arrivo }}</td>
-                        <td>{{ $train->orario_di_partenza }}</td>
-                        <td>{{ $train->orario_di_arrivo }}</td>
-                        <td>{{ $train->codice_treno }}</td>
-                        <td>{{ $train->totale_carrozze }}</td>
-                        <td class="{{ $train->in_orario ? 'bg-success' : 'bg-danger' }}">
+                        <td class="py-5">{{ $train->azienda }}</td>
+                        <td class="py-5">{{ $train->stazione_di_partenza }}</td>
+                        <td class="py-5">{{ $train->stazione_di_arrivo }}</td>
+                        <td class="py-5">{{ $train->orario_di_partenza }}</td>
+                        <td class="py-5">{{ $train->orario_di_arrivo }}</td>
+                        <td class="py-5">{{ $train->codice_treno }}</td>
+                        <td class="py-5">{{ $train->totale_carrozze }}</td>
+                        <td class="{{ $train->in_orario ? 'bg-success' : 'bg-danger' }} py-5">
                             {{ $train->in_orario ? 'Sì' : 'No' }}
                         </td>
-                        <td class="{{ $train->cancellato ? 'bg-danger' : 'bg-success' }}">
+                        <td class="{{ $train->cancellato ? 'bg-danger' : 'bg-success' }} py-5">
                             {{ $train->cancellato ? 'Sì' : 'No' }}</td>
                     </tr>
                 @endforeach
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&family=DynaPuff:wght@400..700&family=Fascinate+Inline&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Nosifer&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Permanent+Marker&family=Roboto:ital,wght@0,100..900;1,100..900&family=Share+Tech+Mono&display=swap"
+        rel="stylesheet">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Trains</title>
 </head>
