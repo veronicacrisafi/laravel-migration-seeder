33c78abb9c6bdae3d1fb0322deccc0aef4534b46
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-18 16:52:27 +0100

    Aggiornato il metodo index nel PageController per recuperare e passare i dati dei treni alla vista

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
+
+use function Symfony\Component\Clock\now;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view("index");
+        $trains = Train::where("orario_di_partenza", '>=', now())->orderBy('orario_di_partenza', 'asc')->get();
+        return view("index", compact('trains'));
     }
 }
