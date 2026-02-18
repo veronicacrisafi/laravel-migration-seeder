917fc2b2f7c7e7fe399a1c18fff9932e87c46cf5
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-18 16:02:08 +0100

    Aggiunta della colonna 'azienda' nella migration della tabella 'trains' e aggiornamento delle specifiche

--- database/migrations/2026_02_18_140648_create_trains_table.php
+++ database/migrations/2026_02_18_140648_create_trains_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             //creazione colonne
+            $table->string('azienda', 50);
             $table->string('stazione_di_partenza', 50);
             $table->string('stazione_di_arrivo', 50);
             $table->dateTime('orario_di_partenza');
