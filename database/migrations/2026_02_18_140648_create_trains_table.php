3a4c64e11bd2980a007fc945beaba82da74dfd6d
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-18 15:28:29 +0100

    Aggiunta delle colonne alla tabella 'trains' nella migration

--- database/migrations/2026_02_18_140648_create_trains_table.php
+++ database/migrations/2026_02_18_140648_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            //creazione colonne
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
+            $table->string('codice_treno', 20);
+            $table->integer('totale_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
