91ae0ce8f28ad31fb414c43a55eed57da44bc975
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-24 19:17:29 +0100

    Aggiunta l'importazione della libreria Faker nel seeder dei treni

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+//import per i faker
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
