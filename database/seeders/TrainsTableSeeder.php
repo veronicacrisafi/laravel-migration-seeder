ed7f365e924e2012ed1047f53b0e7e826daedc4f
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-24 21:37:26 +0100

    aggiunto il ciclo for nel seeder dei treni per generare 20 istanze di treni

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -15,7 +15,20 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        //importo il model e creo l'istanza
-        $newTrain = new Train();
+
+        for ($i = 0; $i > 20; $i++) {
+            //importo il model e creo l'istanza
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->company();
+            $newTrain->stazione_di_partenza = $faker->city();
+            $newTrain->stazione_di_arrivo = $faker->city();
+            $newTrain->orario_di_partenza = $faker->dateTimeBetween('now', '+2 days');
+            $newTrain->orario_di_arrivo = $faker->dateTimeBetween($newTrain->orario_di_partenza, '+4 hours');
+            $newTrain->codice_treno = $faker->bothify('??###');
+            $newTrain->totale_carrozze = $faker->numberBetween(5, 20);
+            $newTrain->in_orario = $faker->boolean();
+            $newTrain->cancellato = $faker->boolean();
+            $newTrain->save();
+        }
     }
 }
